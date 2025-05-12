1220617f70d14b0fa58cc9ec78ab00f438fad571
Pavel Kladkevich <kladweb@gmail.com>
2025-05-12 11:51:28 +0300

    feat: init project

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About | Next App",
+};
+
+export default function About() {
+  return <h3>Select subitem</h3>;
+}
